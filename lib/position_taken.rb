d40fc58342852eb9dcbf4d589c4e56f1546b110e
Patrick Tuszakowski <mrtall500@gmail.com>
2020-10-31 12:51:53 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 def position_taken?(board, index)
-  if board[index] == " " || "" || nil
+  if board[index] == " " || board[index] == "" || board[index] = nil
     return false
+
   elsif board[index] == "X" || board[index] == "O"
-    
     return true
   end
 
